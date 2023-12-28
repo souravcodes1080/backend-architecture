8e23e05e977ae95aaaab32d84db9db4b7360ca60
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2023-12-28 19:15:21 +0530

    util code

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express, { json, urlencoded } from 'express';
+import cors from "cors"
+import cookieParser from 'cookie-parser';
+const app = express(); 
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credential: true
+}))
+
+app.use(express.json({limit: "20kb"}))
+app.use(urlencoded({extended: true, limit: "20kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './db/index.js';
+import {app} from "./app.js"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
 dotenv.config();
-connectDB() 
 
-
-
+connectDB()
+  .then(()=>{
+    app.listen(process.env.PORT || 8080, ()=>{
+        console.log(`Server is running at port ${process.env.PORT}` )
+    })
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed");
+  });
